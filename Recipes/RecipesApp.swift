0823a8e85de1a68456526af44f4dd1796e0edbe4
gaidaasd00 <swiftgaida@list.ru>
2022-09-17 21:56:00 +0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Алексей Гайдуков on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
